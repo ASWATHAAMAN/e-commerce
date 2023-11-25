47329dc328d6f90cbcef63768dc4eeb106c16c1a
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2023-11-25 23:43:49 +0530

    start

---
+++ src/components/navbar.component.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+      <nav className="m-[1rem] ml-[5rem]">
+      <div className="w-[10%] h-[10%] object-contain ">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS989GRiByc43QI2Wn6Ci3r7oarr-aGcZkoFA&usqp=CAU"
+          alt="logo"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export { Navbar };
